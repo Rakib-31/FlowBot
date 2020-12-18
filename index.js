61f6f86299be912bb8cff5955bf835dc33a082a6
Rakib31 <30778508+Rakib31@users.noreply.github.com>
2020-12-18 23:58:47 +0600

    code updated

--- index.js
+++ index.js
@@ -288,42 +288,142 @@ var responseNodes = [
     { id: 18, name: "Super" }
 ];
 
+
+
+function autocomplete(inp, arr) {
+    console.log(arr);
+    var currentFocus;
+    inp.addEventListener("input", function(e) {
+        var a, b, i, val = this.value;
+        console.log(val);
+        /*close any already open lists of autocompleted values*/
+        closeAllLists();
+        if (!val) { return false; }
+        currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/
+        this.parentNode.appendChild(a);
+        for (i = 0; i < arr.length; i++) {
+            console.log(arr[i].question);
+            if (arr[i].question.toUpperCase().includes(val.toUpperCase())) {
+                console.log(arr[i].question);
+                b = document.createElement("DIV");
+                b.innerHTML = arr[i].question;
+                b.innerHTML += "<input type='hidden' value='" + arr[i].question + "'>";
+                b.addEventListener("click", function(e) {
+                    inp.value = this.getElementsByTagName("input")[0].value;
+                    closeAllLists();
+                });
+                a.appendChild(b);
+            }
+        }
+    });
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+            /*If the arrow DOWN key is pressed,
+            increase the currentFocus variable:*/
+            currentFocus++;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 38) { //up
+            /*If the arrow UP key is pressed,
+            decrease the currentFocus variable:*/
+            currentFocus--;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 13) {
+            /*If the ENTER key is pressed, prevent the form from being submitted,*/
+            e.preventDefault();
+            if (currentFocus > -1) {
+                /*and simulate a click on the "active" item:*/
+                if (x) x[currentFocus].click();
+            }
+        }
+    });
+
+    function addActive(x) {
+        /*a function to classify an item as "active":*/
+        if (!x) return false;
+        /*start by removing the "active" class on all items:*/
+        removeActive(x);
+        if (currentFocus >= x.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = (x.length - 1);
+        /*add class "autocomplete-active":*/
+        x[currentFocus].classList.add("autocomplete-active");
+    }
+
+    function removeActive(x) {
+        /*a function to remove the "active" class from all autocomplete items:*/
+        for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active");
+        }
+    }
+
+    function closeAllLists(elmnt) {
+        /*close all autocomplete lists in the document,
+        except the one passed as an argument:*/
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", function(e) {
+        closeAllLists(e.target);
+    });
+}
+
+
+
+
+
+
+
 var searchBar = document.getElementById('search-bar');
 
-searchBar.addEventListener('keyup', (e) => {
-    //searchBar.value = '';
-    let questionBox = document.getElementById('question-box');
-    while (questionBox.firstChild) {
-        questionBox.removeChild(questionBox.lastChild);
-    }
-    let filteredQuestion = questionArray.filter(res => {
-        return res.question.toLowerCase().includes(e.target.value.toLowerCase());
-    });
-    //console.log(filteredQuestion);
-    var div = document.createElement('div');
+// searchBar.addEventListener('keyup', (e) => {
+//     //searchBar.value = '';
+//     let questionBox = document.getElementById('question-box');
+//     while (questionBox.firstChild) {
+//         questionBox.removeChild(questionBox.lastChild);
+//     }
+//     let filteredQuestion = questionArray.filter(res => {
+//         if (e.target.value === '') return '';
+//         return res.question.toLowerCase().includes(e.target.value.toLowerCase());
+//     });
+//     //console.log(filteredQuestion);
+//     var div = document.createElement('div');
 
-    for (let i = 0; i < filteredQuestion.length; i++) {
-        let input = document.createElement('input');
-        input.style.width = '100%';
-        input.style.height = '30px';
-        input.style.background = 'white';
-        input.style.border = '1px solid lightgrey';
-        input.style.textAlign = 'left';
-        input.style.cursor = 'pointer';
-        input.value = filteredQuestion[i].question;
-        input.addEventListener('click', function() {
-            questionHandler(filteredQuestion[i]);
-        }, false);
-        div.appendChild(input);
-    }
+//     for (let i = 0; i < filteredQuestion.length; i++) {
+//         let input = document.createElement('input');
+//         input.style.width = '100%';
+//         input.style.height = '30px';
+//         input.style.background = 'white';
+//         input.style.border = '1px solid lightgrey';
+//         input.style.textAlign = 'left';
+//         input.style.cursor = 'pointer';
+//         input.value = filteredQuestion[i].question;
+//         input.addEventListener('click', function() {
+//             questionHandler(filteredQuestion[i]);
+//         }, false);
+//         div.appendChild(input);
+//     }
 
-    questionBox.appendChild(div);
+//     questionBox.appendChild(div);
 
-    var span = document.getElementsByClassName("close")[0];
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-});
+//     var span = document.getElementsByClassName("close")[0];
+//     span.onclick = function() {
+//         modal.style.display = "none";
+//     }
+// });
 
 const addConditionHandler = (id) => {
     console.log(`add condition handler ${id}`);
@@ -355,10 +455,11 @@ function questionHandler(str) {
 }
 
 const askQuestionHandler = (id) => {
-    let questionBox = document.getElementById('question-box');
-    while (questionBox.firstChild) {
-        questionBox.removeChild(questionBox.lastChild);
-    }
+    // let questionBox = document.getElementById('question-box');
+    // while (questionBox.firstChild) {
+    //     questionBox.removeChild(questionBox.lastChild);
+    // }
+    autocomplete(document.getElementById("search-bar"), questionArray);
     parentId = id;
     searchBar.value = '';
     modal.style.display = "block";
